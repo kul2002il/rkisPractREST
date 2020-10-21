05dc796883390d03c13c98117b74a017f5357780
Ilia <kul.2002.il@gmail.com>
2020-10-21 11:45:48 +0700

    Промежуточное сохранение.

--- app/utilities.py
+++ app/utilities.py
@@ -1,5 +1,9 @@
 from datetime import datetime
 from os.path import splitext
 
+
+# def ///
+
+
 def get_timestamp_path(instance, filename):
-   return '%s%s' % (datetime.now().timestamp(), splitext(filename)[1])
+	return '%s%s' % (datetime.now().timestamp(), splitext(filename)[1])
